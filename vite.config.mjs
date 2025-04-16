cf7764dd0e3333756d2f6960be44ea134ceb827a
Mauricio2085 <maoca2085@gmail.com>
2025-04-16 16:50:41 -0500

    Elimina el archivo de flujo de trabajo de implementación en GitHub Pages y ajusta la configuración de la base en vite.config.mjs.

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mywebsite/", // reemplaza con el nombre exacto de tu repositorio en GitHub
+  base: "/", // reemplaza con el nombre exacto de tu repositorio en GitHub
   plugins: [react()],
 });
