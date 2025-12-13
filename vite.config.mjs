f9e433233f0cdf1ff93475fb831826f2c93d66f9
Mauricio2085 <maoca2085@gmail.com>
2025-12-13 18:00:54 -0500

    chore: update package.json to include Next.js and remove unused dependencies; delete cv-maintenance.js, index.jsx, and vite.config.mjs files

--- vite.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/", // reemplaza con el nombre exacto de tu repositorio en GitHub
-  plugins: [react()],
-});
