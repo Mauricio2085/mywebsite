92357ed190e5ffce4d5841741a16ae49672f46db
Mauricio2085 <maoca2085@gmail.com>
2025-12-13 17:48:30 -0500

    fix: Mejora en la gestión del modo oscuro al inicializar el estado desde localStorage y asegurar el montaje del componente

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,21 +3,30 @@
 import { useEffect, useState } from "react";
 
 const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "dark";
-  });
+  const [isDarkMode, setIsDarkMode] = useState(null);
+  const [isMounted, setIsMounted] = useState(false); // Para saber si ya se montó en el cliente
 
   useEffect(() => {
-    const root = document.documentElement;
-    console.log("Dark mode actualizado:", isDarkMode);
-    if (isDarkMode) {
-      root.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      root.classList.remove("dark");
-      localStorage.setItem("theme", "light");
+    const themeFromStorage = localStorage.getItem("theme");
+    if (themeFromStorage !== null) {
+      setIsDarkMode(themeFromStorage === "dark");
     }
-  }, [isDarkMode]);
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      const root = document.documentElement;
+      console.log("Dark mode actualizado:", isDarkMode);
+      if (isDarkMode) {
+        root.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        root.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+    }
+  }, [isDarkMode, isMounted]);
 
   return [isDarkMode, setIsDarkMode];
 };
